8254f08d30c00614e4afda702173ff14de801844
andxk <kupex@ya.ru>
2025-07-29 01:51:26 +0300

    Initial Commit. Task1

---
+++ src/main/java/ru/netology/Server.java
@@ -0,0 +1,138 @@
+package ru.netology;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class Server {
+    public static final int MAX_THREADS = 64;
+    public final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+    private final int port;
+    private final ExecutorService pool = Executors.newFixedThreadPool(MAX_THREADS);
+//    final ServerSocket serverSocket;
+
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+
+    private void clientHandler(Socket socket) {
+        try (
+                final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                final var out = new BufferedOutputStream(socket.getOutputStream());
+        ) {
+            // read only request line for simplicity
+            // must be in form GET /path HTTP/1.1
+            final var requestLine = in.readLine();
+            if (requestLine == null) return;
+//            if (requestLine == null) continue;
+            final var parts = requestLine.split(" ");
+
+            if (parts.length != 3) {
+                // just close socket
+                return;
+//                continue;
+            }
+
+            final var path = parts[1];
+            if (!validPaths.contains(path)) {
+                out.write((
+                        "HTTP/1.1 404 Not Found\r\n" +
+                                "Content-Length: 0\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.flush();
+                return;
+//                continue;
+            }
+
+            final var filePath = Path.of(".", "public", path);
+            final var mimeType = Files.probeContentType(filePath);
+
+            // special case for classic
+            if (path.equals("/classic.html")) {
+                final var template = Files.readString(filePath);
+                final var content = template.replace(
+                        "{time}",
+                        LocalDateTime.now().toString()
+                ).getBytes();
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + content.length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.write(content);
+                out.flush();
+//                continue;
+                return;
+            }
+
+            final var length = Files.size(filePath);
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            Files.copy(filePath, out);
+            out.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    class ClientThread extends Thread {
+        private Socket socket;
+
+        public ClientThread(Socket socket) {
+            super();
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("client started");
+            clientHandler(socket);
+            System.out.println("   ---client stopped");
+        }
+    }
+
+
+    public void start() {
+        try (final var serverSocket = new ServerSocket(port)) {
+            while (true) {
+                final var socket = serverSocket.accept();
+                pool.submit(new ClientThread(socket));
+//                new ClientThread(socket).start();
+//                    clientHandler(socket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
